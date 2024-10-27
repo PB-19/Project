36751da94b56381f860159503648d5bb993fc9b4
Pranav Balaji <pb19.official@gmail.com>
2024-10-27 13:07:27 +0530

    Twelfth Commit

--- engine/command.py
+++ engine/command.py
@@ -8,6 +8,7 @@ def speak(text):
     engine.setProperty('rate', 125)
     eel.DisplayMessage(text)
     engine.say(text)
+    eel.receiverText(text)
     engine.runAndWait()
 
 def takecommand():
@@ -38,10 +39,10 @@ def allCommands(message=1):
 
     if message==1:
         query = takecommand()
-        print(query)
+        eel.senderText(query)
     else:
         query = message
-        print(query)
+        eel.senderText(query)
 
     try:
         if "open" in query:
