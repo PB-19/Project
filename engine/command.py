71ce86bd032b144500eb699093af5a2308fee429
Pranav Balaji <pb19.official@gmail.com>
2024-10-27 10:29:29 +0530

    Eleventh Commit

--- engine/command.py
+++ engine/command.py
@@ -34,11 +34,16 @@ def takecommand():
     return query.lower()
 
 @eel.expose
-def allCommands():
-    try:
+def allCommands(message=1):
+
+    if message==1:
         query = takecommand()
         print(query)
+    else:
+        query = message
+        print(query)
 
+    try:
         if "open" in query:
             from engine.features import openCommand
             openCommand(query)
