2b6fd5c64ec44d0048afd33ba1e8fd2bb719fed4
Pranav Balaji <pb19.official@gmail.com>
2024-10-28 10:17:22 +0530

    Final Commit

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ import os
 import eel
 from engine.command import *
 from engine.features import *
+from engine.auth import recognize
 
 def start():
     
     eel.init("www")
 
     playAssistantSound()
+
+    @eel.expose
+    def init():
+        subprocess.call([r'device.bat'])
+        eel.hideLoader()
+        speak("Ready for face authentication")
+        flag = recognize.AuthenticateFace()
+        if flag==1:
+            eel.hideFaceAuth()
+            speak("Face authentication successful")
+            eel.hideFaceAuthSuccess()
+            speak("Welcome Sir!")
+            eel.hideStart()
+            playAssistantSound()
+        else: speak("Face authentication failed")
 
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
